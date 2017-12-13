17a31d5c487942ed38f4f59adeec0d0193384cee
Job SAUL <job.saul2@gmail.com>
2017-12-13 09:33:45 +0100

    ADD PROJECT 'Test' WITH Code Bloks

---
+++ CodeBlocks[TEST]/Test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "C'est parti !" << endl;
+    long int i = 0;
+        for(; i < 1000000000; ++i){
+        }
+    cout << "Fini ! : " << i << endl;
+    return 0;
+}
